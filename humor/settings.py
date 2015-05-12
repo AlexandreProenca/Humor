bd6ca2e65b88e87d4750a704b45476a189ed10c6
Alexandre Proença <alexandre.proenca@borayu.com>
2015-05-12 15:31:36 -0300

    first commit

---
+++ humor/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for humor project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'humor'
+
+SPIDER_MODULES = ['humor.spiders']
+NEWSPIDER_MODULE = 'humor.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'humor (+http://www.yourdomain.com)'
